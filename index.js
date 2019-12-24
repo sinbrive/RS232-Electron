b40c123913dddd3587501afcd3d186b32661c6bc
sinbrive <sinbrive@gmail.com>
2019-12-24 22:48:59 +0100

    morin version

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const {app, BrowserWindow}=require("electron");
 //
 const path=require("path");
+const url=require("url");
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({width:800, height:600});
 
     mainWindow.loadFile("index.html");
 
---
+++ main.js
@@ -0,0 +1,39 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 700,
+    height: 500,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  mainWindow.setAutoHideMenuBar(true);
+  mainWindow.setResizable(false);
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
