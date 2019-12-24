619e0dd2426448ca89507f734cce90048ce5d624
sinbrive <sinbrive@gmail.com>
2019-12-24 20:47:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow}=require("electron");
+
+const path=require("path");
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadFile("index.html");
+
+    mainWindow.on("closed", function(){
+        mainWindow=null;
+
+    });
+
+}
+
+app.on("ready", createWindow);
+
